6e62956ed162846f4285e1c97096b9cc12df0c78
Steven Cooke <steve@themuseumcalendar.com>
2015-02-25 03:00:18 -0500

    update all dependencies to 2.4.4 compatible versions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,7 +5,7 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
-
+grails.project.dependency.resolver = "maven"
 grails.server.port.http = 9090
 
 
@@ -37,7 +37,7 @@ grails.project.dependency.resolution = {
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         mavenRepo "http://snapshots.repository.codehaus.org"
         mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
          mavenRepo "http://maven.restlet.org/"
     }
@@ -49,9 +49,9 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:3.6.10.18"
-        runtime ":jquery:1.8.3"
-        runtime ":resources:1.1.6"
+        //runtime ":hibernate:3.6.10.18"
+        runtime ":jquery:1.11.1"
+        runtime ":resources:1.2.14"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
@@ -60,11 +60,12 @@ grails.project.dependency.resolution = {
 
         build ":tomcat:8.0.18.3"
 
-        runtime ":database-migration:1.3.2"
 
-        compile ':cache:1.0.1'
-	compile ':jaxrs:0.8'
+        //runtime ":database-migration:1.4.0"
 
-	compile ':mongodb:3.0.2'
+        compile ':cache:1.1.8'
+	compile ':jaxrs:0.11'
+
+	compile ':mongodb:3.0.1'
     }
 }
