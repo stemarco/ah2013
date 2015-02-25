2b5d4901fa79f8036cc06a41432935fb0192bf6b
Steven Cooke <steve@themuseumcalendar.com>
2015-02-25 01:26:05 -0500

    update dependencies for grails 2.4.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,7 +49,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
+        runtime ":hibernate:3.6.10.18"
         runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
 
@@ -58,13 +58,13 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
 
-        build ":tomcat:$grailsVersion"
+        build ":tomcat:8.0.18.3"
 
         runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
 	compile ':jaxrs:0.8'
 
-	compile ':mongodb:1.2.0'
+	compile ':mongodb:3.0.2'
     }
 }
